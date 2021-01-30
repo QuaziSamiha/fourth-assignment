70a7c017f375753d399d9e13029fbb56aefc55f0
QuaziSamiha <1703155@student.ruet.ac.bd>
2021-01-30 12:23:52 +0600

    first class & economy class plus & minus btn operation is almost done

--- script.js
+++ script.js
@@ -9,9 +9,36 @@ function addButton(buttonId, ticketNumber, perTicket, ticketCost){
     })    
 }
 
-addButton('add-first-class', 'first-ticket-no', 150, 'first-class-cost');
-addButton('add-economy-class', 'economy-ticket-no', 100, 'economy-class-cost');
 
+function subtractButton(buttonId, ticketNumber, perTicket, ticketCost){
+    document.getElementById(buttonId).addEventListener('click', function(){
+        const ticketNo = document.getElementById(ticketNumber).value;
+        const totalTicket = parseInt(ticketNo) - 1;
+        if(totalTicket >= 0){
+            document.getElementById(ticketNumber).value = totalTicket;
+    
+            const totalCost = totalTicket * perTicket;
+            document.getElementById(ticketCost).innerText = totalCost;
+        }
+    })    
+}
+
+addButton('add-first-class', 'first-ticket-no', 150, 'first-class-cost');
+subtractButton('sub-first-class', 'first-ticket-no', 150, 'first-class-cost')
+
+addButton('add-economy-class', 'economy-ticket-no', 100, 'economy-class-cost');
+subtractButton('sub-economy-class', 'economy-ticket-no', 100, 'economy-class-cost');
+
+// document.getElementById('sub-first-class').addEventListener('click', function(){
+//     const ticketNo = document.getElementById('first-ticket-no').value;
+//     const totalTicket = parseInt(ticketNo) - 1;
+//     if(totalTicket >= 0){
+//         document.getElementById('first-ticket-no').value = totalTicket;
+
+//         const totalCost = totalTicket * 150;
+//         document.getElementById('first-class-cost').innerText = totalCost;
+//     }
+// })
 
 
 
